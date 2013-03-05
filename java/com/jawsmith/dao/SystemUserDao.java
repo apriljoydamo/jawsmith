3a37e88edecb64016435fb00a230a86c9370dc2b
apriljoydamo <apriljoydamo@gmail.com>
2013-03-05 08:21:57 -0800

    system user data access object

--- java/com/jawsmith/dao/SystemUserDao.java
+++ java/com/jawsmith/dao/SystemUserDao.java
@@ -1,5 +1,38 @@
 package com.jawsmith.dao;
 
-public class SystemUserDao {
+import java.util.List;
 
+import com.jawsmith.interfaces.DataAccesses;
+
+public class SystemUserDao extends HibernateDaoSupport implements DataAccesses{
+
+	@Override
+	public void save(Object obj) {
+		getHibernateTemplate().save(obj);
+		
+	}
+
+	@Override
+	public void update(Object obj) {
+		getHibernateTemplate().update(obj);
+		
+	}
+
+	@Override
+	public void delete(Object obj) {
+		getHibernateTemplate().delete(obj);
+		
+	}
+
+	@Override
+	public Object findById(int i) {
+		List list = getHibernateTemplate().find("from users where USER_ID=?",i);
+		return list.get(0);
+	}
+
+	@Override
+	public List getAll() {
+		List list = getHibernateTemplate().find("from users");
+		return list;
+	}
 }
